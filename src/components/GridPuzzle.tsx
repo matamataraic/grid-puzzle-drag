5e975f4c0b626b5e3a47a50c023048d85b38f39e
matamataraic <matamataraic@gmail.com>
2025-02-17 12:53:40 +0100

    Update GridPuzzle.tsx

--- src/components/GridPuzzle.tsx
+++ src/components/GridPuzzle.tsx
@@ -45,7 +45,7 @@ export const GridPuzzle = () => {
   const [showWarning, setShowWarning] = useState(false);
   const [showFieldsWarning, setShowFieldsWarning] = useState(false);
   const [orderType, setOrderType] = useState<"regular" | "magnetic">("regular");
-  const [headerImage, setHeaderImage] = useState('https://i.imgur.com/R4Hgigd.jpeg');
+  const [headerImage, setHeaderImage] = useState('https://i.imgur.com/dOTt71G.jpeg');
   const [orderForm, setOrderForm] = useState<OrderFormData>({
     name: '',
     address: '',
